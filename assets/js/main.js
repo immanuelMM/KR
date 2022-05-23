8aafc84befb4722bc47334e0d3fd8e6100c8f600
mawe <immanuel@qstrike.com>
2022-05-23 11:01:12 -0400

    fic links

--- assets/js/main.js
+++ assets/js/main.js
@@ -251,5 +251,46 @@
       mirror: false
     })
   });
-
+  
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById("myImg");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+  // QR
+  // Get the modal
+  var modal1 = document.getElementById("myModal123");
+  
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img1 = document.getElementById("myImg123");
+  var modalImg1 = document.getElementById("img01");
+  var captionText1 = document.getElementById("caption");
+  img1.onclick = function(){
+    modal1.style.display = "block";
+    modalImg1.src = this.src;
+    captionText1.innerHTML = this.alt;
+  }
+  
+  // Get the <span> element that closes the modal
+  var span1 = document.getElementsByClassName("close1")[0];
+  
+  // When the user clicks on <span> (x), close the modal
+  span1.onclick = function() { 
+    modal1.style.display = "none";
+  }
 })()
