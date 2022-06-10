4393793375418509838f019261b847a2d1c5b598
mawe <immanuel@qstrike.com>
2022-06-10 02:12:50 -0400

    fic links

--- assets/js/main.js
+++ assets/js/main.js
@@ -227,17 +227,36 @@
   });
 
   new Swiper('.portfolio-details-slider1', {
-    speed: 1000,
-    loop: true,
-    autoplay: {
-      delay: 1000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   /**
